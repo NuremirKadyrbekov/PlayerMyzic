1036fff89f4fc8cf0699193a3115d34105ad0870
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2023-10-09 22:58:41 +0600

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    id: 1,
+    link: "beat1.mp3",
+    genre: "Rap",
+    track: "Я солдат",
+    group: "Nizza",
+    year: 2019,
+    
+  },
+  {
+    id: 2,
+    link: "beat2.mp3",
+    genre: "Xip-Xop ",
+    track: "Тает Лёд",
+    group: "Грибы",
+    year: 2021,
+  },
+  {
+    id: 3,
+    link: "beat3.mp3",
+    genre: "POP",
+    track: "Мокрые кроссы",
+    group: "Тима Беларуских",
+    year: 2018,
+    
+  },
+  {
+    id: 4,
+    link: "beat4.mp3",
+    genre: "Rap",
+    track: "The Search",
+    group: "NF",
+    year: 2018,
+  },
+  {
+    id: 5,
+    link: "beat5.mp3",
+    genre: "Cantry",
+    track: "Белая ночь",
+    group: "Виктор Цой",
+    year: 2018,
+  },
+  {
+    id: 5,
+    link: "beat6.mp3",
+    genre: "Cantry",
+    track: "Слышишь ты для меня целый мир",
+    group: "Nurico",
+    year: 2023,
+  },
+];
